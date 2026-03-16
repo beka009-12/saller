aaf9a3a2406b4a41ef7f15f71dd10b9f0c287e8d
Bekbol <bekbol@MacBook-Air-Bekbol.local>
2026-03-16 15:44:59 +0600

    .

--- src/provider/Provider.tsx
+++ src/provider/Provider.tsx
@@ -1,18 +1,28 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { FC, ReactNode } from "react";
+import { FC, ReactNode, useState } from "react"; // Добавили useState
 
 interface IProviderProps {
   children: ReactNode;
 }
 
 const Provider: FC<IProviderProps> = ({ children }) => {
-  const queryClient = new QueryClient();
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  );
+
   return (
     <QueryClientProvider client={queryClient}>
       {children}
-      <ReactQueryDevtools initialIsOpen={true} />
+      <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
   );
 };
